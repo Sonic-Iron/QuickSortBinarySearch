e3d67f9e7d4c054bd83f816d75d57a00ac432faf
Sonic-Iron <sam@thehewittfamily.com>
2024-03-12 19:48:20 +0000

    somehow an error?

--- QuickSortBinarySearch/QuickSortBinarySearch.cpp
+++ QuickSortBinarySearch/QuickSortBinarySearch.cpp
@@ -16,12 +16,30 @@ std::vector<int> createData(const int LENGTH, const int MAXSIZE)
     return dataSet;
 }
 
-std::pair<std::vector<int>, std::vector<int>> quickSort(std::vector<int> subset)
+std::vector<int> quickSort(std::vector<int> subset)
 {
-    if (subset.size() < 2)
+    int pivot = subset.at(subset.size() / 2);
+    std::vector<int> v1, v2;
+    for (int element : subset)
     {
-
+        if (element <= pivot)
+        {
+            v1.push_back(element);
+        }
+        else{
+            v2.push_back(element);
+        }
     }
+    if (v1.size() > 1)
+    {
+        std::vector<int> v1 = quickSort(v1);
+    }
+    if (v2.size() > 1)
+    {
+        std::vector<int> v2 = quickSort(v2);
+    }
+    v1.insert(v1.end(), v2.begin(), v2.end()); //how does this modify v1 without creating a new variable?
+    return v1;
 }
 
 
@@ -32,7 +50,22 @@ int main()
     const int MAXSIZE = 100;
     const int MAXLENGTH = 10;
     const int sizeOfArray = std::rand() % MAXLENGTH;
+
     std::vector<int> dataSet = createData(MAXLENGTH, MAXSIZE);
+
+    for (int i : dataSet)
+    {
+        std::cout << i;
+    }
+    std::cout << std::endl;
+
+    dataSet = quickSort(dataSet);
+    
+    for (int i : dataSet)
+    {
+        std::cout << i;
+    }
+    std::cout << std::endl;
 }
 
 
