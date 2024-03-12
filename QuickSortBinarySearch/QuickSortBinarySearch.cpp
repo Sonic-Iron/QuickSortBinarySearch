e75d45a319731d931a6b729c15521bac3da47dbc
Sonic-Iron <sh2611@york.ac.uk>
2024-03-12 16:09:13 +0000

    added createData

--- QuickSortBinarySearch/QuickSortBinarySearch.cpp
+++ QuickSortBinarySearch/QuickSortBinarySearch.cpp
@@ -6,8 +6,14 @@
 #include <vector>
 
 
-std::vector<int> createData(const int n)
+std::vector<int> createData(const int LENGTH, const int MAXSIZE)
 {
+    std::vector<int> dataSet;
+    for (int i = 0; i < LENGTH, i++)
+    {
+        dataSet.push_back(std::rand() % MAXSIZE);
+    }
+    return dataSet;
 
 }
 
