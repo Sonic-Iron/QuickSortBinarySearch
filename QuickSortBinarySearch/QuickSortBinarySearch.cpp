3f2d767668cc187b9e76a07321f9e23e52e7e302
Sonic-Iron <sh2611@york.ac.uk>
2024-03-12 18:49:49 +0000

    started quciksort

--- QuickSortBinarySearch/QuickSortBinarySearch.cpp
+++ QuickSortBinarySearch/QuickSortBinarySearch.cpp
@@ -9,19 +9,30 @@
 std::vector<int> createData(const int LENGTH, const int MAXSIZE)
 {
     std::vector<int> dataSet;
-    for (int i = 0; i < LENGTH, i++)
+    for (int i = 0; i < LENGTH; i++)
     {
         dataSet.push_back(std::rand() % MAXSIZE);
     }
     return dataSet;
-
 }
+
+std::pair<std::vector<int>, std::vector<int>> quickSort(std::vector<int> subset)
+{
+    if (subset.size() < 2)
+    {
+
+    }
+}
+
+
+
 
 int main()
 {
-    const int maxSizeOfArray = 10;
-    const int sizeOfArray = std::rand() % maxSizeOfArray;
-    createData()
+    const int MAXSIZE = 100;
+    const int MAXLENGTH = 10;
+    const int sizeOfArray = std::rand() % MAXLENGTH;
+    std::vector<int> dataSet = createData(MAXLENGTH, MAXSIZE);
 }
 
 
